fada519668dee335badd826081cd908e24746144
Fernando <fernando.mkv@gmail.com>
2023-08-30 17:50:03 -0300

    consertando erro em msg formulário

--- js/formulario.js
+++ js/formulario.js
@@ -3,7 +3,7 @@ const form = document.querySelector("form");
 const formEnviado = (response) => {
   if (response.ok) {
     form.innerHTML =
-      "<p class='font-2-l' style='grid-column: 1/-1; padding: 1rem; border-radius: 4px; background-color: #f7f7f7;'><span style='color:#317A00'>Mensagem enviada<span/>, em breve entraremos em contato. Geralmente respondemos em 24 horas.</p>";
+      "<p class='font-2-l' style='grid-column: 1/-1; padding: 1rem; border-radius: 4px; background-color: #f7f7f7;'><span style='color:#317A00'>Mensagem enviada</span>, em breve entraremos em contato. Geralmente respondemos em 24 horas.</p>";
   } else {
     form.innerHTML =
       "<p class='font-2-l' style='grid-column: 1/-1; padding: 1rem; border-radius: 4px; background-color: #f7f7f7;'><span style='color:#e00000'>Erro no envio</span>, você pode enviar diretamente para o nosso email em: contato@bikcraft.shop</p>";
